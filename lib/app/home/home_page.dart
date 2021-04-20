5ac8c7b5de65cc61788a51555f606695b87289a4
BrendowLincoln <linconlbrendow@gmail.com>
2021-04-20 19:47:07 -0300

    Aula 2 - Level Button

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:dev_quiz/app/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/app/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:dev_quiz/app/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            LevelButtonWidget(label: "Fácil",),
+            LevelButtonWidget(label: "Médio",),
+            LevelButtonWidget(label: "Difícil",),
+            LevelButtonWidget(label: "Perito",),
+          ],
+        ),
+      ),
     );
   }
 }
